219aa08ea4bafe4b4cd9b53be39e8d4d5b343d8e
elliniS <ellini.vs@aluno.ifsc.edu.br>
2023-04-16 21:32:35 -0300

    Atividade ListView Frutas

---
+++ ListViewFrutas/ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    FrutaController frutaController;
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView tvNome = view.findViewById(R.id.tvNome);
+                String nome = tvNome.getText().toString();
+
+                Toast.makeText(getApplicationContext(), nome, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        frutaController = new FrutaController();
+        atualizaLista();
+    }
+
+    public void atualizaLista(){
+        listView.setAdapter(
+                new ArrayFrutaAdapter(
+                        getApplicationContext(),
+                        R.layout.item_lista,
+                        frutaController.FRUTAS
+                )
+        );
+    }
+}
